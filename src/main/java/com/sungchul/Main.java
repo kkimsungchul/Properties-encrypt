e38388367926b0af6644e2a77044a642c4af6821
kimsc1218 <kimsc1218@gmail.com>
2026-09-22 19:31:09 +0900

    2026.09.22

--- src/main/java/com/sungchul/Main.java
+++ src/main/java/com/sungchul/Main.java
@@ -3,6 +3,7 @@ package com.sungchul;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
 import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
 import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig;
 
 import java.io.File;
@@ -15,24 +16,47 @@ public class Main {
     static SimpleStringPBEConfig config;
 
     public static void main(String[] args) {
+
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setPassword("ktds.dsquare");
+        encryptor.setAlgorithm("PBEWITHMD5ANDDES");
+        String dbUrl = "aH/kWzNb9qofLgeddkEHYE27ABguUk7tLVOd7U5NX0nVE+kxPJTK73T6db/gH61X66mGL/sbUmw=";
+        String dbId = "XpiFHrrQ4fw+MX/+gK3q8P89zB4j94b9";
+        String dbPw = "OwLkYz9XbIHE4XKXoDJCAQ==";
+
+        try {
+            String decryptedTextDbUrl = encryptor.decrypt(dbUrl);
+            System.out.println("decryptedTextDbUrl Text: " + decryptedTextDbUrl);
+            String decryptedTextDbId = encryptor.decrypt(dbId);
+            System.out.println("decryptedTextDbId Text: " + decryptedTextDbId);
+            String decryptedTextDbPw = encryptor.decrypt(dbPw);
+            System.out.println("decryptedTextDbPw Text: " + decryptedTextDbPw);
+        } catch (Exception e) {
+            System.err.println("Decryption failed: " + e.getMessage());
+        }
+
+        System.out.println(encryptor.encrypt("jdbc:postgresql://192.168.55.125:5532/dsquare"));
+        System.out.println(encryptor.encrypt("dsquare_user"));
+
+
         initConfig();
 //        String databaseIP = "192.168.0.22";
 //        String encryptData =  encrypt("databaseIP",databaseIP);
 //        String decryptData = decrypt("databaseIP",encryptData);
-        String filePath = "dev.yaml";
-        ClassLoader classLoader = Main.class.getClassLoader();
-        File file = new File(classLoader.getResource(filePath).getFile());
-        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
-        System.out.println("file path : " + filePath);
-        try{
-            Map<String,Object> map = objectMapper.readValue(file, Map.class);
-            for (Map.Entry<String, Object> entry : map.entrySet()) {
-                String decryptData = encrypt(entry.getKey() , entry.getValue().toString());
-                //decrypt(entry.getKey() , decryptData);
-            }
-        }catch (IOException e){
-            e.printStackTrace();
-        }
+//        String filePath = "dev.yaml";
+//        ClassLoader classLoader = Main.class.getClassLoader();
+//        File file = new File(classLoader.getResource(filePath).getFile());
+//        ObjectMapper objectMapper = new ObjectMapper(new YAMLFactory());
+//        System.out.println("file path : " + filePath);
+//        try{
+//            Map<String,Object> map = objectMapper.readValue(file, Map.class);
+//            for (Map.Entry<String, Object> entry : map.entrySet()) {
+//                String decryptData = encrypt(entry.getKey() , entry.getValue().toString());
+//                //decrypt(entry.getKey() , decryptData);
+//            }
+//        }catch (IOException e){
+//            e.printStackTrace();
+//        }
     }
 
 
